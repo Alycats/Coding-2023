dacc02acbac0c613aacaae5a0146ab18e017ab91
Alycat <Arc183@student.aru.ac.uk>
2023-01-19 14:23:00 +0000

    first commit

---
+++ learningcsharp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace learngcsharp1;
+class Program
+{
+    static void Main(string[] args)
+    {
+string username;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please Enter Your Name");
+        username = Console.ReadLine();
+        Consoleg.WriteLine($"Goodbye, {username}!");
+        Console.ReadKey(true);
+    }
+}
